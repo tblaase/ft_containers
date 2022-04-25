f436fbc4c63c4c65f4c5fd83933455311ec729c8
tblaase <tblaase@student.42heilbronn.de>
2022-04-25 18:42:25 +0200

    new main, for testing the original behaviour of vector STL

--- src/main.cpp
+++ src/main.cpp
@@ -1,116 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 17:32:13 by tblaase           #+#    #+#             */
+/*   Updated: 2022/04/25 18:25:31 by tblaase          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
-#include <string>
-#include <deque>
-#if 1 //CREATE A REAL STL EXAMPLE
-	#include <map>
-	#include <stack>
-	#include <vector>
-	namespace ft = std;
-#else
-	#include <map.hpp>
-	#include <stack.hpp>
-	#include <vector.hpp>
-#endif
+#include <vector>
 
-#include <stdlib.h>
-
-#define MAX_RAM 4294967296
-#define BUFFER_SIZE 4096
-struct Buffer
+int main()
 {
-	int idx;
-	char buff[BUFFER_SIZE];
-};
+	std::vector<char> a;
+
+	// std::cout << "size: " << a.size() << std::endl;
+	// std::cout << "capacity: " << a.capacity() << std::endl;
+	a.reserve(4);
+	std::cout << "size: " << a.size() << std::endl;
+	std::cout << "capacity: " << a.capacity() << std::endl;
+
+	for (int i = 0; i < 7; i++)
+		a.push_back(i + 32);
+
+	std::cout << "size: " << a.size() << std::endl;
+	std::cout << "capacity: " << a.capacity() << std::endl;
+
+	// for (int i = a.size(); i < 96; i++)
+	// 	a.push_back(i);
+
+	// std::cout << "size: " << a.size() << std::endl;
+	// std::cout << "capacity: " << a.capacity() << std::endl;
 
 
-#define COUNT (MAX_RAM / (int)sizeof(Buffer))
-
-template<typename T>
-class MutantStack : public ft::stack<T>
-{
-public:
-	MutantStack() {}
-	MutantStack(const MutantStack<T>& src) { *this = src; }
-	MutantStack<T>& operator=(const MutantStack<T>& rhs) 
-	{
-		this->c = rhs.c;
-		return *this;
+	try{
+		a.resize(15);
 	}
-	~MutantStack() {}
-
-	typedef typename ft::stack<T>::container_type::iterator iterator;
-
-	iterator begin() { return this->c.begin(); }
-	iterator end() { return this->c.end(); }
-};
-
-int main(int argc, char** argv) {
-	if (argc != 2)
+	catch (std::exception &e)
 	{
-		std::cerr << "Usage: ./test seed" << std::endl;
-		std::cerr << "Provide a seed please" << std::endl;
-		std::cerr << "Count value:" << COUNT << std::endl;
-		return 1;
-	}
-	const int seed = atoi(argv[1]);
-	srand(seed);
-
-	ft::vector<std::string> vector_str;
-	ft::vector<int> vector_int;
-	ft::stack<int> stack_int;
-	ft::vector<Buffer> vector_buffer;
-	ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
-	ft::map<int, int> map_int;
-
-	for (int i = 0; i < COUNT; i++)
-	{
-		vector_buffer.push_back(Buffer());
+		std::cerr << e.what() <<std::endl;
 	}
 
-	for (int i = 0; i < COUNT; i++)
-	{
-		const int idx = rand() % COUNT;
-		vector_buffer[idx].idx = 5;
-	}
-	ft::vector<Buffer>().swap(vector_buffer);
+	std::cout << "size: " << a.size() << std::endl;
+	std::cout << "capacity: " << a.capacity() << std::endl;
 
-	try
-	{
-		for (int i = 0; i < COUNT; i++)
-		{
-			const int idx = rand() % COUNT;
-			vector_buffer.at(idx);
-			std::cerr << "Error: THIS VECTOR SHOULD BE EMPTY!!" <<std::endl;
-		}
-	}
-	catch(const std::exception& e)
-	{
-		//NORMAL ! :P
-	}
-	
-	for (int i = 0; i < COUNT; ++i)
-	{
-		map_int.insert(ft::make_pair(rand(), rand()));
-	}
+	std::cout << "max: " << a.max_size() << std::endl;
 
-	int sum = 0;
-	for (int i = 0; i < 10000; i++)
-	{
-		int access = rand();
-		sum += map_int[access];
-	}
-	std::cout << "should be constant with the same seed: " << sum << std::endl;
+	for (size_t i = 0; i < a.size(); i++)
+		std::cout << (int)a[i] << std::endl;
 
-	{
-		ft::map<int, int> copy = map_int;
-	}
-	MutantStack<char> iterable_stack;
-	for (char letter = 'a'; letter <= 'z'; letter++)
-		iterable_stack.push(letter);
-	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
-	{
-		std::cout << *it;
-	}
-	std::cout << std::endl;
+	std::cout << "size: " << a.size() << std::endl;
+	std::cout << "capacity: " << a.capacity() << std::endl;
+
 	return (0);
 }
