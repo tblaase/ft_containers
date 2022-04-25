93487e5472c611ba3514a2f8b20e3609d55f8c72
tblaase <tblaase@student.42heilbronn.de>
2022-04-25 18:41:30 +0200

    interface for iterators added

--- include/utils/iterator.hpp
+++ include/utils/iterator.hpp
@@ -1,18 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   random_access_iterator.hpp                         :+:      :+:    :+:   */
+/*   iterator.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 12:02:50 by tblaase           #+#    #+#             */
-/*   Updated: 2022/04/24 12:19:46 by tblaase          ###   ########.fr       */
+/*   Updated: 2022/04/25 18:36:23 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <cstddef>
+# include <cstddef>
+
+// this is just an interface
 
 namespace ft
 {
@@ -20,35 +22,41 @@ namespace ft
 		struct iterator
 		{
 			typedef T			value_type;
-			typedef Distance	diff_type;
+			typedef Distance 	difference_type;
 			typedef Pointer		pointer;
 			typedef Reference	reference;
-			typedef Category	it_category;
+			typedef Category	iterator_category;
 		};
 
-	struct input_iterator_tag {};
-	struct output_iterator_tag {};
-	struct forward_iterator_tag: public input_iterator_tag {};
-	struct bidirectional_iterator_tag: public forward_iterator_tag {};
-	struct random_access_iterator_tag: public bidirectional_iterator_tag {};
+	// Iterator tags
+	struct input_iterator_tag
+	{};
+	struct output_iterator_tag
+	{};
+	struct forward_iterator_tag: public input_iterator_tag
+	{};
+	struct bidirectional_iterator_tag: public forward_iterator_tag
+	{};
+	struct random_access_iterator_tag: public bidirectional_iterator_tag
+	{};
 
-	template<class Iterator>
+	template < class Iterator >
 		struct iterator_traits
 		{
-			typedef typename Iterator::value_type	value_type;
-			typedef typename Iterator::diff_type	diff_type;
-			typedef typename Iterator::pointer		pointer;
-			typedef typename Iterator::reference	reference;
-			typedef typename Iterator::it_category	it_category;
+			typedef typename Iterator::difference_type		difference_type;
+			typedef typename Iterator::value_type			value_type;
+			typedef typename Iterator::pointer				pointer;
+			typedef typename Iterator::reference			reference;
+			typedef typename Iterator::iterator_category	iterator_category;
 		};
 
-	template<class T>
+	template < class T >
 		struct iterator_traits<T*>
 		{
 			typedef T							value_type;
 			typedef value_type*					pointer;
-			typedef value_type&					referance;
-			typedef ptrdiff_t					diff_type;
-			typedef random_access_iterator_tag	it_category;
+			typedef value_type&					reference;
+			typedef ptrdiff_t					difference_type;
+			typedef random_access_iterator_tag	iterator_category;
 		};
 }
